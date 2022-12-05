1c0557c39bfa8a93fffbdc5edd140036ceb0f212
martapla <martaplanella@hotmail.com>
2022-12-05 19:32:47 +0100

    first commit

---
+++ src/components/SignIn.vue
@@ -0,0 +1,56 @@
+<!-- COMPONENTE BOILERPLATE -->
+ 
+  <template>
+
+  <div class="container">
+    <h3 class="header-title">Log In to ToDo App</h3>
+    <p class="header-subtitle">
+      <!-- Estamos en la ruta de login. Aquí deberíais crear un form con la lógica correspondiente para que este permita al usuario loguearse con su email y su contraseña. Miraros la lógica de SignUp si necesitáis inspiración :) -->
+    </p>
+
+    <form @submit.prevent="signUp" class="form-sign-in">
+      <div class="form">
+          <div class="form-input">
+            <label class="input-field-label">E-mail</label>
+            <input
+              type="email"
+              class="input-field"
+              placeholder="example@gmail.com"
+              id="email"
+              v-model="email"
+              required
+            />
+          </div>
+          <div class="form-input">
+            <label class="input-field-label">Password</label>
+            <input
+              type="password"
+              class="input-field"
+              placeholder="**********"
+              id="password"
+              v-model="password"
+              required
+            />
+          </div>
+        </div>  
+      </form>
+
+    <p>Dont have an account? <PersonalRouter :route="route" :buttonText="buttonText" class="sign-up-link"/></p>
+  </div>
+
+</template>
+
+<script setup>
+import PersonalRouter from "./PersonalRouter.vue";
+
+// Route Variables
+const route = "/auth/signup";
+const buttonText = "Sign Up";
+
+// Arrow function to Signin user to supaBase
+const signIn = async () => {
+  try {} catch (error) {}
+};
+</script>
+
+<style></style>
